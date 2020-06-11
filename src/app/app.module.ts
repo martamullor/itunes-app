74084b722d0b01fbd83c9d1e49e26de786df5749
martamullor <marta.mullor.polo@gmail.com>
2020-06-11 12:54:57 +0200

    nav structure without functionality done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IonSearchbarComponent } from './components/ion-searchbar/ion-searchbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IonSearchbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/ion-searchbar/ion-searchbar.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'ion-searchbar',
+  templateUrl: './ion-searchbar.component.html',
+  styleUrls: ['./ion-searchbar.component.css']
+})
+export class IonSearchbarComponent implements OnInit {
+
+  ngOnInit() {}
+  
+}
